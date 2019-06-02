0edf2fab6c411055788b43c571642840cb828dea
swathi-0901 <swathi.kasikala@gmail.com>
2019-06-02 12:22:38 +0530

    done with the changes regading

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+#what to add something regarding controllers
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
